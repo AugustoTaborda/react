906853b1b5f34f8840ca289ad64bf459d6fc3db3
Augusto Taborda <augustotaborda14@gmail.com>
2024-05-29 20:36:35 -0300

    React

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, {useState,useEffect}from "react";
 import { Navibar } from "../../components/NaviBar";
+import axios from "axios";  
 
 export function Home() {
- return (
- <div>
-   <Navibar path='/' element={<Navibar />}/>
-   <h1>oi</h1>
- </div>
- );
  
+  const[pokemon, setPokemon] = useState([]);
+  
+  useEffect(()=>{
+    const fetchPokemon = async () => {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        setPokemon(response.data.results);
+        console.log(pokemon)
+      }catch (error){
+        console.log(error);
+      }
+    }
+
+    fetchPokemon();
+  },[]);
+
+ return (<>
+   <div>
+      <Navibar>
+      </Navibar>
+    </div> 
+    <div>
+    
+    </div>
+
+    <table>
+    <tbody>
+        <tr>  
+          <td>{
+            pokemon.map((poke) => {return <tr>{poke.name}</tr>})
+            }</td>
+            <td>{
+              pokemon.map((poke) => {return <tr>{poke.url}</tr>})
+              }
+            </td>
+        </tr> 
+    </tbody>
+    </table>
+</>
+ )
 }
